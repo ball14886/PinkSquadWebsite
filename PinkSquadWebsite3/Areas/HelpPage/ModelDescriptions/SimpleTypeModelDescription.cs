71d1b6071b391abe4dc905230eb11f7e060f04af
Jirasin Jirasakul <jirasin@genxas.com>
2018-10-21 13:40:02 +0700

    init

---
+++ PinkSquadWebsite3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PinkSquadWebsite3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PinkSquadWebsite3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PinkSquadWebsite3.WebApiApplication" Language="C#" %>
